26924383023f2e14d6f095da9c3ad7e2e03b95be
Mehedi Hasan <mehedihasansustcse889@gmail.com>
2026-02-12 14:44:21 +0600

    Model loading functinality added.

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,19 @@
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, Start, END 
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+
+class GraphBuilder():
+    def __inti__(self):
+        pass 
+
+    def agent_functin(self):
+        pass 
+
+    def build_graph(self):
+        pass 
+
+    def __call__(self):
+        pass
